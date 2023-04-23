e33c91947f693bd43ebce686190d4578df29843d
quadfault <quadfault@gmail.com>
2023-04-23 18:36:58 -0400

    Some directory reorganization.
    Also added index.html, for easier example traversal.

---
+++ engine/engine.js
@@ -0,0 +1,11 @@
+/* engine.js - A WebGL graphics engine.
+ * Written by quadfault
+ * 3/11/23
+ */
+
+export * from './renderer.js'
+export * from './scene.js'
+export * from './node.js'
+export * from './mesh.js'
+export * from './math.js'
+export * from './camera.js'
